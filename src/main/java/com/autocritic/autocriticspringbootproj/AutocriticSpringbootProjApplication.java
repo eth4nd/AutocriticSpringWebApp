666d092d94039cb47a6d2893e0ce271fc98b5660
Akshay Khalasi <akshay.khalasi3@gmail.com>
2021-11-04 15:49:30 -0700

    first commit

---
+++ src/main/java/com/autocritic/autocriticspringbootproj/AutocriticSpringbootProjApplication.java
@@ -0,0 +1,13 @@
+package com.autocritic.autocriticspringbootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocriticSpringbootProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocriticSpringbootProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autocritic/autocriticspringbootproj/AutocriticSpringbootProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocritic.autocriticspringbootproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocriticSpringbootProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
