317500a6daa6a640f82d7e5c3669cec756e8a5bd
Akshay Khalasi <akshay.khalasi3@gmail.com>
2021-11-04 17:00:04 -0700

    created AmazonConfig file

---
+++ src/main/java/com/autocritic/autocriticspringbootproj/config/AmazonConfig.java
@@ -0,0 +1,20 @@
+package com.autocritic.autocriticspringbootproj.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    public AmazonS3 s3(){
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+           "AKIAQNUHEPFEGYHK6KU6","46fiA27SLsAU0zp7N6TgmOYJ7hV7xKZXdS/Gx/lP"
+        );
+        return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+    }
+
+}
