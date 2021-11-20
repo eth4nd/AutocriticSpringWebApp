2824426693508a903c898a0b695b6ebb6251c68e
David Sang <dvdsang47@gmail.com>
2021-11-19 22:38:08 -0800

    added javadoc comments to User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,7 +5,12 @@ public class User {
     private String username;
     private int password;
 
-
+    /**
+     * Constructor for User objects
+     * @param userID The ID of the User
+     * @param username The User's username
+     * @param password The User's password
+     */
     public User(UUID userID, String username, int password){
         if (password < -1 || password > 999999999) {
             throw new IllegalArgumentException("Password is invalid; not within set boundary");
@@ -15,18 +20,35 @@ public class User {
         this.username = username;
     }
 
+    /**
+     * Getter method for password
+     * @return password
+     */
     public int getPassword() {
         return password;
     }
 
+    /**
+     * Getter method for username
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Getter method for userID
+     * @return userID
+     */
     public UUID getUserID() {
         return userID;
     }
 
+    /**
+     * Deep equals method for User objects
+     * @param o User object
+     * @return True if contains same password, userID, and username
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -35,6 +57,10 @@ public class User {
         return password == user.password && Objects.equals(userID, user.userID) && Objects.equals(username, user.username);
     }
 
+    /**
+     * HashCode method that generates a hashCode based on userID, username, and password
+     * @return int
+     */
     @Override
     public int hashCode() {
         return Objects.hash(userID, username, password);
