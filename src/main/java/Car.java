a05726f1e6d26e7d26b3c6ec3a8577d9a013ccbb
David Sang <dvdsang47@gmail.com>
2021-11-19 22:32:39 -0800

    added javadoc comments to Car class

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -3,6 +3,13 @@ public class Car {
     private double price;
     private double averageRating;
 
+    /**
+     * Constructor for Car objects, Cars should be unique
+     *
+     * @param carDescription The description of the car
+     * @param price The price of the car
+     * @param averageRating The average rating of the car
+     */
     public Car (String carDescription, double price, double averageRating)
     {
         this.carDescription = carDescription;
@@ -10,16 +17,31 @@ public class Car {
         this.averageRating = averageRating;
     }
 
+    /**
+     * Getter method for carDescription
+     *
+     * @return carDescription
+     */
     public String getCarDescription()
     {
         return this.carDescription;
     }
 
+    /**
+     * Getter method for price
+     *
+     * @return price
+     */
     public double getPrice()
     {
         return this.price;
     }
 
+    /**
+     * Getter method for averageRating
+     *
+     * @return averageRating
+     */
     public double getAverageRating()
     {
         return this.averageRating;
