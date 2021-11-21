6c17361ace1dfd99a7b004c7b7f41a34caad2ed9
Ethan <ethan@MacBook-Pro-2.local>
2021-11-21 00:18:05 -0800

    added javadoc for Review class

--- src/main/java/Model/Review/Review.java
+++ src/main/java/Model/Review/Review.java
@@ -11,6 +11,13 @@ public class Review {
     private Car car;
     private double rating;
 
+    /**
+     * Constructor to create Review object
+     * @param review review for car
+     * @param user user account for review
+     * @param car car being reviewed
+     * @param rating rating for the car review
+     */
     public Review(String review, User user, Car car, double rating) {
         this.review = review;
         this.user = user;
@@ -20,23 +27,41 @@ public class Review {
 
 
     @Override
+    /**
+     * output of the review in String form
+     */
     public String toString() {
         return "Rating: "  + rating + ", Review: " + review + " User: " + user.getUsername();
     }
 
+    /**
+     * output of car review
+     * @return this.review
+     */
     public String getReview() {
         return this.review;
     }
 
-
+    /**
+     * getter method of associated user for car review
+     * @return this.user
+     */
     public User getUser() {
         return this.user;
     }
 
+    /**
+     * getter method of associated car for car review
+     * @return this.car
+     */
     public Car getCar() {
         return this.car;
     }
 
+    /**
+     * getter method of rating for car review
+     * @return this.rating
+     */
     public double getRating() {
         return this.rating;
     }
