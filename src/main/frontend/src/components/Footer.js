483508409910037b8f543fa88be628e1e5336079
David Sang <dvdsang47@gmail.com>
2021-11-23 13:15:59 -0800

    started up ReactJS and added frontend components

---
+++ src/main/frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p className='classSection'><b>Fall 2021 CS 151 Section 8</b></p>
+            <p className='groupNames'>Akshay Khalasi, Ethan DeGuzman, Minh Nguyen, David Sang</p>
+        </div>
+    )
+}
+
+export default Footer
