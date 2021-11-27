d7f15dff38362c31aeb8a6e6d3cc8772973d65d5
David Sang <dvdsang47@gmail.com>
2021-11-26 17:15:05 -0800

    added Home page view and Car cards

--- src/main/frontend/src/helpers/CarList.js
+++ src/main/frontend/src/helpers/CarList.js
@@ -1,8 +1,25 @@
 //Test car
+import Honda from '../assets/2022_Honda_Civic_Type_R.jpg';
+
 export const CarList = [
     {
         name: '2022 Honda Civic Type-R',
-        image: null,
+        image: Honda,
         price: 21700
+    },
+    {
+        name: 'Car2',
+        image: null,
+        price: 200
+    },
+    {
+        name: 'Car3',
+        image: null,
+        price: 300
+    },
+    {
+        name: 'Car4',
+        image: null,
+        price: 400
     }
 ]
--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { CarList } from '../helpers/CarList'
+import CarCard from '../components/CarCard'
+import '../styles/Home.css'
 
 function Home() {
     return (
         <div className='home'>
             <div className='headerContainer'>
-                <li>TestItem</li>
-                <h1>A list of cars will be here</h1>
-                <p>You are at Home</p>
+                <p>You are at the Home page</p>
+                <h1>Choose a car to review</h1>
+            </div>
+            <div className='carList'>
+                {CarList.map((car, key) => {
+                    return (
+                        <CarCard
+                            key={key}
+                            image={car.image}
+                            name={car.name}
+                            price={car.price}
+                        />
+                    )
+                })}
             </div>
         </div>
     )
