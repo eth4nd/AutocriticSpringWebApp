48a1172bd99eb10190eb9fd2a1782b19799f7596
Minh Nguyen <mnguyen4725@gmail.com>
2021-11-28 01:05:24 -0800

    added json package

--- src/main/view/src/Components/Text/ReviewTextArea.js
+++ src/main/view/src/Components/Text/ReviewTextArea.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 import React, {useState, useEffect} from "react";
 
 const ReviewTextArea = ()=>{
-    const url = "http://localhost:8080/api/v1/review/review/upload"
+    const url = "http://localhost:8080/api/v1/review/upload"
     //data object declared
     const [data,setData] = useState({
         review: "",
