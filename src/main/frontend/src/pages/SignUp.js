c6f9e68f17db81d11b43b11cabb3a2d51283713b
David Sang <dvdsang47@gmail.com>
2021-11-29 21:01:19 -0800

    updated Sign Up page

--- src/main/frontend/src/pages/SignUp.js
+++ src/main/frontend/src/pages/SignUp.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import '../styles/SignUp.css'
 
 function SignUp() {
     return (
         <div className='signup'>
             <div className='headerContainer'>
                 <p>You are at the Sign Up page</p>
-                <h1>A sign up menu will be here</h1>
+                <h1>Sign up to start reviewing</h1>
+            </div>
+            <div className='menuContainer'>
+                <form className='signupMenu' action='/'>
+                    <p>Don't have an account? Sign up below.</p>
+                    <label htmlFor='username'>Username</label>
+                    <input type='text' id='username' />
+                    <label htmlFor='password'>Password</label>
+                    <input type='text' id='password' />
+                    <input className='signupButton' type='submit' value='SIGN UP' />
+                </form>
+                <form className='loginMenu' action='/'>
+                    <p>Already have an account? Log in below.</p>
+                    <label htmlFor='username'>Username</label>
+                    <input type='text' id='username' />
+                    <label htmlFor='password'>Password</label>
+                    <input type='text' id='password' />
+                    <input className='loginButton' type='submit' value='LOG IN' />
+                </form>
             </div>
         </div>
     )
