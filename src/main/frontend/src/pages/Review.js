8c1e39554f571a4d86d3bb3675571bed1a4b03b4
Ethan <ethan@MacBook-Pro-2.local>
2021-11-29 16:41:43 -0800

    added Review temp page

---
+++ src/main/frontend/src/pages/Review.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Review() {
+    return (
+        <div className='review'>
+            <div className='headerContainer'>
+                <p>You are at the Review page</p>
+                <h1>A review menu will be here</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Review
