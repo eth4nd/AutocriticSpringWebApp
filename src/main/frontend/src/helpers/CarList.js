4a98141818c8879db380f74ff7fbc00ba945b491
Ethan <ethan@MacBook-Pro-2.local>
2021-11-29 16:38:08 -0800

    added car images and modified respective data

--- src/main/frontend/src/helpers/CarList.js
+++ src/main/frontend/src/helpers/CarList.js
@@ -1,5 +1,8 @@
 //Test car
 import Honda from '../assets/2022_Honda_Civic_Type_R.jpg';
+import Tesla from '../assets/2022-tesla-model-3.jpg';
+import Toyota from '../assets/2022_toyota_camry.jpg';
+import Mercedes from '../assets/2022-mercedes-benz-c-class.jpg';
 
 export const CarList = [
     {
@@ -8,18 +11,18 @@ export const CarList = [
         price: 21700
     },
     {
-        name: 'Car2',
-        image: null,
-        price: 200
+        name: '2022 Tesla Model 3',
+        image: Tesla,
+        price: 43990
     },
     {
-        name: 'Car3',
-        image: null,
-        price: 300
+        name: '2022 Toyota Camry',
+        image: Toyota,
+        price: 25295
     },
     {
-        name: 'Car4',
-        image: null,
-        price: 400
+        name: '2022 Mercedes-Benz C-Class',
+        image: Mercedes,
+        price: 42000
     }
 ]
